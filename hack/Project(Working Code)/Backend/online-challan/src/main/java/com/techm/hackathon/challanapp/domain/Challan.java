827ff6ea88436d0868e82609a476c0c70e93422d
asus <sc00351054@techmahindra.com>
2017-09-24 11:57:10 +0530

    papu aaya

--- hack/Project(Working Code)/Backend/online-challan/src/main/java/com/techm/hackathon/challanapp/domain/Challan.java
+++ hack/Project(Working Code)/Backend/online-challan/src/main/java/com/techm/hackathon/challanapp/domain/Challan.java
@@ -1,15 +1,11 @@
 package com.techm.hackathon.challanapp.domain;
 
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
